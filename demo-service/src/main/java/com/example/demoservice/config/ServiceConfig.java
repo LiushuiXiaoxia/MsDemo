1e634f10f0293a1a6c2a569e968e5a4353677d7f
xiaqiulei <xiaqiulei@126.com>
2019-12-23 12:24:26 +0800

    consul as config

---
+++ demo-service/src/main/java/com/example/demoservice/config/ServiceConfig.java
@@ -0,0 +1,13 @@
+package com.example.demoservice.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "config")
+public class ServiceConfig {
+
+    String message;
+}
--- demo-service/src/main/java/com/example/demoservice/controller/ServiceController.java
+++ demo-service/src/main/java/com/example/demoservice/controller/ServiceController.java
@@ -1,24 +1,38 @@
 package com.example.demoservice.controller;
 
+import com.example.demoservice.config.ServiceConfig;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
 @RequestMapping(("/test"))
+@RefreshScope
 public class ServiceController {
 
     @Value("${server.port}")
     int port;
 
-    @RequestMapping
-    public String test() {
-        String s = "Hello world form demo service." + port;
+    @Autowired
+    ServiceConfig config;
 
-        log.info(s);
+    @RequestMapping(method = RequestMethod.GET)
+    public Object test() {
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put("msg", "Hello world form demo service");
+        map.put("port", port);
+        map.put("config", config);
 
-        return s;
+        log.info("map = {}", map);
+
+        return map;
     }
 }
